2000db193bcd314604324152e5f1c9363f4463a6
hihi333444jj <friendcodechaos@gmail.com>
2026-01-03 00:16:01 -0800

    Update Create_Shape.py

--- HelperScripts/Create_Shape.py
+++ HelperScripts/Create_Shape.py
@@ -6,7 +6,7 @@ def Rect(X, Y, width, height, fill=(0,0,0), border=None, borderWidth=2,
      opacity=255, rotateAngle=0, align='left-top',
      Screen = screen,render = False):
     
-    #Create rectangle/frame
+    #Create rectangle/framee
     if border == None:
         TempSurf = pygame.Surface((width, height), pygame.SRCALPHA)
         #set graident texture size
@@ -215,3 +215,4 @@ def Polygon(*args,
         Screen.blit(TempSurf, rect)
     else:
         return(TempSurf,rect)
+
