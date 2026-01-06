a4c86ec11ab3c5864a6df275b74d2a5de3ad9ca6
hihi333444jj <friendcodechaos@gmail.com>
2026-01-06 10:38:48 -0800

    Upload files via upload.py

--- HelperScripts/ManageSound.py
+++ HelperScripts/ManageSound.py
@@ -17,35 +17,5 @@ def DeleteSound(*args):
         #list.pop(numb) removes the list[numb] from the list and if no number givven removes the last value
         var.Sounds[0].pop(Del)
         var.Sounds[1].pop(Del)
-def AddMusic(*args):
-    for item in args:
-        path = Path(item)
-
-        if path.is_dir():
-            for file in path.iterdir():
-                if file.is_file():
-                    var.MusicPlaylist.append(str(file))
-
-        elif path.is_file():
-            var.MusicPlaylist.append(str(path))
-    print(var.MusicPlaylist)
-def Music():
-    pos = 0
-    while True:
-        if len(var.MusicPlaylist) == 0:
-            sleep(.1)
-        else:
-            if pos >= len(var.MusicPlaylist):
-                pos = 0
-            else:
-                pos += 1
-            mixer.music.load(var.MusicPlaylist[pos])
-            mixer.music.play(loops=0, start=0.0, fade_ms = 5000)
-            song = var.MusicPlaylist[pos]
-            sound = mixer.Sound(song)
-            length = sound.get_length()
-            print(length)
-            sleep(length-1)
-
 
 # endregion
