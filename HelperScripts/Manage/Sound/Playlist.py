fcc4ae68bbce4d76c7cc681cce2adb509e813cb0
hihi333444jj <friendcodechaos@gmail.com>
2026-01-08 00:48:51 -0800

    Upload files via upload.py

--- HelperScripts/Manage/Sound/Playlist.py
+++ HelperScripts/Manage/Sound/Playlist.py
@@ -2,29 +2,30 @@ import HelperScripts.GlobalVars as var
 from HelperScripts.Manage.Sound.Music import Add as MusicAdd
 from pathlib import Path
 
+Playlists = [[],[]] # this will store [playlist,Name] playlist is just an array of sound files
 
 def Load(Playlist, Random = False):
-    Playlist = var.Playlists[1].index(Playlist)
-    MusicAdd(*var.Playlists[0][Playlist], Random = Random)
+    Playlist = Playlists[1].index(Playlist)
+    MusicAdd(*Playlists[0][Playlist], Random = Random)
 
 
 def Add(*Song, Name = None):
     if Name == None:
         print("Error no name for playlist")
     else:
-        var.Playlists[0].append([])
-        var.Playlists[1].append(Name)
-        PlaylistSpot = var.Playlists[1].index(Name)
+        Playlists[0].append([])
+        Playlists[1].append(Name)
+        PlaylistSpot = Playlists[1].index(Name)
         for Song in Song:
-            var.Playlists[0][PlaylistSpot].append(Song)
+            Playlists[0][PlaylistSpot].append(Song)
 
 def Remove(*args):
     for i in args:
-        Del = var.Playlists[1].index(i)
+        Del = Playlists[1].index(i)
         #list.remove(Var) deletes the item that is called Var from list
         #list.pop(numb) removes the list[numb] from the list and if no number givven removes the last value
-        var.Playlists[0].pop(Del)
-        var.Playlists[1].pop(Del)
+        Playlists[0].pop(Del)
+        Playlists[1].pop(Del)
 
 def AddFolder(*Folder, Name = None):
     if Name == None:
@@ -39,12 +40,32 @@ def AddFolder(*Folder, Name = None):
                         songs.append(str(file))
     Add(*songs,Name=Name)
 
+def RemoveFolder(*Folder, Name = None):
+    if Name == None:
+        print("Error no name for playlist")
+    else:
+        songs = []
+        for Folders in Folder:
+            path = Path(Folders)
+            if path.is_dir():
+                for file in path.iterdir():
+                    if file.is_file():
+                        songs.remove(str(file))
+
 def AddSong(*Song,Playlist = None):
-    Playlist = var.Playlists[1].index(Playlist)
+    Playlist = Playlists[1].index(Playlist)
     for song in Song:
-        var.Playlists[0][Playlist].append(song)
+        Playlists[0][Playlist].append(song)
 
 def RemoveSong(*Song,Playlist = None):
-    Playlist = var.Playlists[1].index(Playlist)
+    if Playlist == None:
+        print("No plalist removed")
+    else:
+        PlaylistNumb = Playlists[1].index(Playlist)
+        for song in Song:
+            Playlists[0][PlaylistNumb].index(song)
+
+def RemoveSong(*Song,Playlist = None):
+    Playlist = Playlists[1].index(Playlist)
     for song in Song:
-        var.Playlists[0][Playlist].pop(song)
+        Playlists[0][Playlist].pop(song)
