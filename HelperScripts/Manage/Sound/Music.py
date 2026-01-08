9810d1bca18bbe3f4dbdf064ca35c3918fe4b249
hihi333444jj <friendcodechaos@gmail.com>
2026-01-08 00:48:51 -0800

    Upload files via upload.py

--- HelperScripts/Manage/Sound/Music.py
+++ HelperScripts/Manage/Sound/Music.py
@@ -4,15 +4,17 @@ from time import sleep
 from pathlib import Path
 from random import randint
 # region Music
+Fade = 2
+CurrentPlaylist = []
 def Randomise(Playlist = None):
     if Playlist == None:
-        MusicToAdd = var.MusicPlaylist
+        MusicToAdd = CurrentPlaylist
         MusicOrderNew = []
-        for i in range(0,len(var.MusicPlaylist)):
+        for i in range(0,len(CurrentPlaylist)):
             rand = randint(0,len(MusicToAdd)-1)
             MusicOrderNew.append(MusicToAdd[rand])
             MusicToAdd.pop(rand)
-        var.MusicPlaylist = MusicOrderNew
+        CurrentPlaylist = MusicOrderNew
 def Add(*musics,Random=False):
     for item in musics:
         path = Path(item)
@@ -20,35 +22,38 @@ def Add(*musics,Random=False):
         if path.is_dir():
             for file in path.iterdir():
                 if file.is_file():
-                    var.MusicPlaylist.append(str(file))
+                    CurrentPlaylist.append(str(file))
 
         elif path.is_file():
-            var.MusicPlaylist.append(str(path))
+            CurrentPlaylist.append(str(path))
     if Random == True:
         Randomise()
-    print("music",var.MusicPlaylist)
+    print("music",CurrentPlaylist)
 
 def Remove(*args):
     for i in args:
-        var.MusicPlaylist.index(i)
+        CurrentPlaylist.index(i)
+def RemoveAll():
+        CurrentPlaylist = []
 
 def Music():
     pos = -1
     while True:
-        if len(var.MusicPlaylist) == 0:
+        TrueFade = Fade*1000
+        if len(CurrentPlaylist) == 0:
             sleep(.1)
         else:
-            if pos >= len(var.MusicPlaylist):
+            if pos >= len(CurrentPlaylist)-1:
                 pos = 0
             else:
                 pos += 1
-            mixer.music.load(var.MusicPlaylist[pos])
-            mixer.music.play(loops=0, start=0.0, fade_ms = 5000)
-            song = var.MusicPlaylist[pos]
+            mixer.music.load(CurrentPlaylist[pos])
+            mixer.music.play(loops=0, start=0.0, fade_ms = TrueFade)
+            song = CurrentPlaylist[pos]
             sound = mixer.Sound(song)
             length = sound.get_length()
             print(length)
-            sleep(length-1)
+            sleep(length-Fade)
 
 
 # endregion
